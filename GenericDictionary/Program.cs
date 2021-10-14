bbd11e14b11ff9fe60599414e1077a5cd72159d9
vishal.Alok <you@example.com>
2021-10-14 11:55:15 +0530

    Add project files.

---
+++ GenericDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, object> MyDictionary = new Dictionary<string, object>();
+            MyDictionary.Add("Name", "Vishal");
+            MyDictionary.Add("Age", 24);
+            MyDictionary.Add("IsMarried", false);
+
+            foreach (string s in MyDictionary.Keys)
+                Console.WriteLine(s + " : " + MyDictionary[s]);
+            Console.ReadLine();
+        }
+    }
+}
